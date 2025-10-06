c8050f8512b1db586debc3151791c101125f5b54
qou2 <85427247+qou2@users.noreply.github.com>
2025-10-06 21:29:45 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import XboxAccounts from "./pages/XboxAccounts";
+import Downloads from "./pages/Downloads";
+import Instructions from "./pages/Instructions";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +20,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/xbox-accounts" element={<XboxAccounts />} />
+          <Route path="/downloads" element={<Downloads />} />
+          <Route path="/instructions" element={<Instructions />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
